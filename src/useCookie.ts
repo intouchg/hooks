643cbba82778cc5426cf3c3b6437ec059cdd0f00
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2021-08-01 23:01:36 -0500

    Stop encoding/decoding URI component in useCookie, bump to v0.0.13

--- src/useCookie.ts
+++ src/useCookie.ts
@@ -4,13 +4,13 @@ export const useCookie = (key: string) => {
     const [ cookie, setCookie ] = useState<any>()
 
     const getCookie = useCallback(() => {
-        const cookie = document.cookie.split(';').find(cookieString => decodeURIComponent(cookieString).includes(key))
+        const cookie = document.cookie.split(';').find(cookieString => cookieString.includes(key))
         const cookieValue = cookie ? cookie.replace(key + '=', '').trimStart() : null
         return cookieValue ? JSON.parse(cookieValue) : cookieValue
     }, [ key ])
 
     const setCookieCallback = useCallback((value, maxAgeSeconds?: number) => {
-        document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(JSON.stringify(value))}${maxAgeSeconds ? `;max-age=${maxAgeSeconds}` : ''}`
+        document.cookie = `${key}=${JSON.stringify(value)}${maxAgeSeconds ? `;max-age=${maxAgeSeconds}` : ''}`
         setCookie(getCookie())
     }, [ key ])
 
